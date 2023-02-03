96846a6bbdf5ce63dc8088a600e4a5ac8b23ad9e
rajashri03 <telvekar.rajashri@gmail.com>
2023-02-03 11:54:44 +0530

    Rajashri-Added pair intersection

---
+++ LogicalPrograms/PairIntersection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    internal class PairIntersection
+    {
+        public static void arrayInput(int[] a)
+        {
+            Console.WriteLine("Enter the pair of Elements:");
+            for (int j = 0; j <= 1; j++)
+            {
+                a[j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public static int[] Pair_Intersection(int[] Pair1, int[] Pair2)
+        {
+
+            int[] Pair3 = new int[2];
+            int[] Pair4 = new int[Pair1.Length + Pair2.Length];
+            
+            if (Pair2[0] <= Pair1[1])
+            {
+                Pair3[0] = Pair2[0];
+                Pair3[1] = Pair1[1];
+                return Pair3;
+            }
+            else
+            {
+                Console.WriteLine("They dont have similar values");
+            }
+            return null;
+        }
+
+
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -6,12 +6,19 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            VendingMachine machine = new VendingMachine();
-            machine.MinNumberOfNote(5);
+            //VendingMachine machine = new VendingMachine();
+            //machine.MinNumberOfNote(5);
 
-            string str = "XYZ";
-            int n = str.Length;
-            Permutation.permute(str, 0, n - 1);
+            //string str = "XYZ";
+            //int n = str.Length;
+            //Permutation.permute(str, 0, n - 1);
+
+            int[] Pair1 = new int[2];
+            int[] Pair2 = new int[2];
+            PairIntersection.arrayInput(Pair1);
+            PairIntersection.arrayInput(Pair2);
+            int[] resultPair = PairIntersection.Pair_Intersection(Pair1, Pair2);
+            Console.WriteLine("[{0}]", string.Join(", ", resultPair));
         }
     }
 }
